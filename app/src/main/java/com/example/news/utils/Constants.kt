088da7e036da374c1e1361ba27fb9ecc5d3a1e5a
ArturKrasnoshchok <arturr2016@ukr.net>
2023-01-08 21:51:51 +0200

    added DI and retrofit API

---
+++ app/src/main/java/com/example/news/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.news.utils
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "f58778521f3e4d12a5e88bf3870b576e"
+
+    }
+}
